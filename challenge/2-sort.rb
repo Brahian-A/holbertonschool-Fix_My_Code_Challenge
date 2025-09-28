ea38545ff47b3c855bd0fb10dbbf87c1f8cab53d
Brahia-A <10114@holbertonstudents.com>
2025-09-28 20:40:24 -0300

    task 2 ruby

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -6,26 +6,26 @@
 
 result = []
 ARGV.each do |arg|
-  # skip if not integer
-  next if arg !~ /^-?[0-9]+$/
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
 
-  i_arg = arg.to_i
-
-  # insertion sort
-  is_inserted = false
-  i = 0
-  l = result.size
-  while !is_inserted && i < l
-    if result[i] < i_arg
-      i += 1
-    else
-      result.insert(i, i_arg)   # <- correcto (sin - 1)
-      is_inserted = true
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
     end
-  end
-
-  result << i_arg unless is_inserted
+    result << i_arg if !is_inserted
 end
 
-# un número por línea
 puts result
